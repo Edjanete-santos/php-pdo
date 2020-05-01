aec84158574fd4752984f42db5259d09fa270388
Edjanete-santos <52841833+Edjanete-santos@users.noreply.github.com>
2020-05-01 11:24:53 -0300

    Add files via upload

---
+++ update.php
@@ -0,0 +1,29 @@
+<?php
+include './conexao.php';
+
+$sql = "UPDATE produtos SET 
+        descricao = :desc, 
+        quantidade = :qtd, 
+        valor = :val 
+        WHERE id = :id";
+
+     $stmt = $conexao->prepare($sql);
+
+     $descricao = 'Placa mãe';
+     $quantidade = 7;
+     $valor = 780;
+     $id = 2;
+
+     $stmt->bindParam(':desc',$descricao);
+     $stmt->bindParam(':qtd',$quantidade);
+     $stmt->bindParam(':val',$valor);
+     $stmt->bindParam(':id',$id);
+    /*$stmt->bindValue(1, 'Monitor');
+     $stmt->bindValue(2, 17);
+     $stmt->bindParam(3, 299);*/
+
+     $stmt->execute();
+     /*$a = 'Pdot';
+    
+
+?>
